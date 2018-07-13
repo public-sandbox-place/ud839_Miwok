e32a909144b70b5c2ef5a11de92b9c150363886f
public.sandbox.place <public.sandbox.place@gmail.com>
2018-07-13 11:00:39 +0200

    First ArrayAdapter implementation

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,13 +32,21 @@ public class NumbersActivity extends AppCompatActivity {
 
         Log.v("Main Activity", words.toString());
 
-        LinearLayout numberList = findViewById(R.id.numbers_list);
+        ListView listView = findViewById(R.id.numbers_list);
 
+/*
+        Not optimal approach for the long list!!!
+        --------------------------------------------------
         for (int index = 0; index < words.size(); index++) {
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             numberList.addView(wordView);
         }
+*/
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, words
+        );
 
+        listView.setAdapter(itemsAdapter);
     }
 }
