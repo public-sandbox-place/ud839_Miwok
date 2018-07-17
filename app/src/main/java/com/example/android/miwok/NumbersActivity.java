d24a124a164dcd4d0e9339420c06725b9ecd7525
public.sandbox.place <public.sandbox.place@gmail.com>
2018-07-17 16:14:53 +0200

    AudioManager Back to Parent activity Push/click feedback ripple effect

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,22 +1,25 @@
 package com.example.android.miwok;
 
+
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
+
+    private AudioManager mAudioManager;
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,17 +51,108 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView adapterView, View view, int position, long id) {
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, words.get(position).getSong());
-                mMediaPlayer.start();
+                releaseMediaPlayer();
+                int result = mAudioManager.requestAudioFocus(
+                        mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT
+                );
+                Log.v("MainActivity", "FOCUS VALUE IS: " + result);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    Log.v("MainActivity", "AUDIO GRANTED");
+                    //mAudioManager.registerMediaButtonEventReceiver(RemoteControlReceiver);
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, words.get(position).getSong());
+                    mMediaPlayer.start();
+                }
+
 
                 mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mp) {
                         Toast.makeText(NumbersActivity.this, "I FINISHED", Toast.LENGTH_SHORT).show();
+                        releaseMediaPlayer();
+
+
                     }
                 });
                 Toast.makeText(NumbersActivity.this, words.get(position).toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        mOnAudioFocusChangeListener =
+                new AudioManager.OnAudioFocusChangeListener() {
+                    public void onAudioFocusChange(int focusChange) {
+
+                        Log.v("MainActivity", "onAudioFocusChange: " + focusChange);
+                        if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                            mMediaPlayer.pause();
+                            mMediaPlayer.seekTo(0);
+                            // Pause playback because your Audio Focus was
+                            // temporarily stolen, but will be back soon.
+                            // i.e. for a phone call
+                        } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                            // Stop playback, because you lost the Audio Focus.
+                            // i.e. the user started some other playback app
+                            // Remember to unregister your controls/buttons here.
+                            // And release the kra — Audio Focus!
+                            // You’re done.
+                            releaseMediaPlayer();
+                        } else if (focusChange ==
+                                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                            // Lower the volume, because something else is also
+                            // playing audio over you.
+                            // i.e. for notifications or navigation directions
+                            // Depending on your audio playback, you may prefer to
+                            // pause playback here instead. You do you.
+                        } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                            mMediaPlayer.start();
+                            // Resume playback, because you hold the Audio Focus
+                            // again!
+                            // i.e. the phone call ended or the nav directions
+                            // are finished
+                            // If you implement ducking and lower the volume, be
+                            // sure to return it to normal here, as well.
+                        }
+                    }
+                };
+
+
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
 }
